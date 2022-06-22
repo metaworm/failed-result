9158c54c601a9daa1f5fe3133b23955eb8c06862
metaworm <metaworm@outlook.com>
2022-06-22 14:57:45 +0800

    HRESULT & NTSTATUS support for windows

--- src/windows.rs
+++ src/windows.rs
@@ -1,6 +1,10 @@
 //! implements [`LastError`] for common types on Windows
 
 use super::*;
+use ::windows::core::HRESULT;
+use ::windows::Win32::Foundation::NTSTATUS;
+
+pub use ::windows::core::Result as WindowsResult;
 
 macro_rules! impl_int {
     ($t:ty) => {
@@ -21,3 +25,35 @@ macro_rules! impl_int {
 impl_int!(u8);
 impl_int!(i32);
 impl_int!(u32);
+
+pub trait AsHResult {
+    fn as_hresult(self) -> WindowsResult<()>;
+}
+
+impl AsHResult for HRESULT {
+    fn as_hresult(self) -> WindowsResult<()> {
+        self.ok()
+    }
+}
+
+impl AsHResult for i32 {
+    fn as_hresult(self) -> WindowsResult<()> {
+        HRESULT(self).as_hresult()
+    }
+}
+
+pub trait NtStatusResult {
+    fn ntstatus_result(self) -> WindowsResult<()>;
+}
+
+impl NtStatusResult for NTSTATUS {
+    fn ntstatus_result(self) -> WindowsResult<()> {
+        self.to_hresult().ok()
+    }
+}
+
+impl NtStatusResult for i32 {
+    fn ntstatus_result(self) -> WindowsResult<()> {
+        NTSTATUS(self).ntstatus_result()
+    }
+}
